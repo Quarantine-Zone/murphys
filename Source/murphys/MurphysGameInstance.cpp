ebe2f78318e35e9232218265a2d136be9cb97bcd
JT Salisbury <crazyscouter1@gmail.com>
2020-05-04 15:10:38 -0400

    Begin the search results

--- Source/murphys/MurphysGameInstance.cpp
+++ Source/murphys/MurphysGameInstance.cpp
@@ -3,7 +3,7 @@
 
 #include "MurphysGameInstance.h"
 #include "OnlineSessionSettings.h"
-#include "Public/Interfaces/OnlineSessionInterface.h"
+#include "ARSharedWorldPlayerController.h"
 #include "Engine/Engine.h"
 
 const static FName SESSION_NAME = TEXT("Test Session");
@@ -25,6 +25,14 @@ void UMurphysGameInstance::Init() {
 		if (SessionInterface.IsValid()) {
 			SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UMurphysGameInstance::OnCreateSessionComplete);
 			SessionInterface->OnDestroySessionCompleteDelegates.AddUObject(this, &UMurphysGameInstance::OnDestroySessionComplete);
+			SessionInterface->OnJoinSessionCompleteDelegates.AddUObject(this, &UMurphysGameInstance::OnJoinSessionComplete);
+			SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this, &UMurphysGameInstance::OnFindSessionComplete);
+
+			SessionSearch = MakeShareable(new FOnlineSessionSearch());
+			if (SessionSearch.IsValid()) {
+				//SessionSearch->bIsLanQuery = true;
+				SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
+			}
 		}
 	}
 }
@@ -52,6 +60,9 @@ void UMurphysGameInstance::CreateSession() {
 
 	// Create a new session
 	FOnlineSessionSettings settings;
+	settings.bIsLANMatch = true;
+	settings.NumPublicConnections = 2;
+	settings.bShouldAdvertise = true;
 	SessionInterface->CreateSession(0, SESSION_NAME, settings);
 }
 
@@ -79,3 +90,47 @@ void UMurphysGameInstance::OnCreateSessionComplete(FName SessionName, bool Succe
 	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
 }
 
+void UMurphysGameInstance::Join(uint32 Index) {
+	if (!SessionInterface.IsValid() || SessionSearch.IsValid()) {
+		return;
+	}
+
+	SessionInterface->JoinSession(0, SESSION_NAME, SessionSearch->SearchResults[Index]);
+}
+
+void UMurphysGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result) {
+	if (!SessionInterface.IsValid()) {
+		return;
+	}
+	
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) {
+		return;
+	}
+
+	FString Address;
+	if (!SessionInterface->GetResolvedConnectString(SessionName, Address)) {
+		UE_LOG(LogTemp, Warning, TEXT("Could not get connect string"));
+		return;
+	}
+
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Joining %s"), *Address);
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) {
+		return;
+	}
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+}
+
+void UMurphysGameInstance::OnFindSessionComplete(bool Success) {
+	if (!Success || !SessionSearch.IsValid()) {
+		return;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Find sessions completed"));
+	for (const FOnlineSessionSearchResult& SearchResult : SessionSearch->SearchResults) {
+		UE_LOG(LogTemp, Warning, TEXT("Found session named %s"), *SearchResult.GetSessionIdStr());
+	}
+}
--- Source/murphys/MurphysGameInstance.h
+++ Source/murphys/MurphysGameInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "OnlineSubsystem.h"
+#include "Public/Interfaces/OnlineSessionInterface.h"
 #include "Engine/GameInstance.h"
 #include "MurphysGameInstance.generated.h"
 
@@ -18,13 +19,18 @@ class MURPHYS_API UMurphysGameInstance : public UGameInstance
 	
 	void OnCreateSessionComplete(FName SessionName, bool Success);
 	void OnDestroySessionComplete(FName SessionName, bool Success);
+	void OnFindSessionComplete(bool Success);
+
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
 
 	void CreateSession();
 
 	IOnlineSessionPtr SessionInterface;
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
 
 public:
 	void Host();
+	void Join(uint32 Index);
 
 	void MurphysGameInstance(const FObjectInitializer& ObjectInitializer);
 	virtual void Init();
